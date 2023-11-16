0d8451a2dde78d2a3793e60d847bb2e251310448
CS0000 <chenshuosusan@163.com>
2023-11-16 14:25:52 +0100

    update filtering bar plot: ant-tree

--- helper.py
+++ helper.py
@@ -7,6 +7,11 @@ from collections import Counter
 import folium
 from streamlit_folium import st_folium
 import pickle
+import gspread 
+import json
+from streamlit_gsheets import GSheetsConnection
+
+
 
 
 color_map = {'Agave': '#d5de81',
@@ -48,6 +53,11 @@ metabolic_template = dict(layout=go.Layout(font_family='Roboto',
                                            font=dict(size=18),
                                            xaxis=dict(automargin=True),
                                            ))
+metabolic_colors_20 = ['#a63246' ,'#e56e61' ,'#ea9243' ,'#ffc840' ,'#789b40' ,'#bdd576','#f6e67f' ,'#2db77f' ,'#61c6c0' ,'#61cbe8' ,'#d87fae' ,'#9772a3' ,'#1f6585' ,'#b6517f' ,'#a89778' ,'#eae4b1' ,'#a2a9ad' ,'#a65b4a' ,'#82766a' ,'#797c82']
+metabolic_colors_40 = ['#a63246' ,'#cd3e49' ,'#e56e61' ,'#d16d29' ,'#ea9243' ,'#d8a83b' ,'#ffc840' ,'#5f8943' ,'#789b40' ,'#87bf65' ,'#bdd576' ,'#e2e26a' ,'#f6e67f' ,'#288e5b' ,'#2db77f' ,'#43a89a' ,'#61c6c0' ,'#a9dde3' ,'#61cbe8' ,'#60adcd' ,'#d87fae' ,'#a4a9d5' ,'#9772a3' ,'#775285' ,'#1f6585' ,'#2e839e' ,'#b6517f' ,'#a09256' ,'#a89778' ,'#c4b36a' ,'#eae4b1' ,'#f5f3ea' ,'#a2a9ad' ,'#b7744f' ,'#a65b4a' ,'#7b5d52' ,'#82766a' ,'#8c9380' ,'#797c82']
+
+cred_path = './credentials.json'
+
 
 # load data
 p = pd.read_csv('./Phase1_processed_dataset_pressures - Long_data_with_pressures_v2.csv')
@@ -61,6 +71,78 @@ all_col = test1.columns.tolist()
 
 with open('./geo_country_new.plk','rb') as f:
     l = pickle.load(f)
+
+
+def sheet_url_split(s_in):
+    sheet_id = s_in.split('https://docs.google.com/spreadsheets/d/')[1].split('/')[0]
+    return sheet_id
+
+def read_sheet(sheet_id,sheet_name):
+    sheet_name = sheet_name.replace(' ','%20')
+    url_df = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    df = pd.read_csv(url_df,engine='python')
+    return df
+
+@st.cache_resource()
+def get_google_sheet_connection(sheet_name):
+    with open(cred_path) as j_f:
+        cred = json.load(j_f)
+    gc, authorized_user = gspread.oauth_from_dict(cred)
+    sh = gc.open(sheet_name)  # spreadsheet name  "Phase1_pressures"
+    return sh
+
+@st.cache_data()
+def read_credentials_sheet(sheet_name, worksheet_id):
+    sh = get_google_sheet_connection(sheet_name)
+    worksheet = sh.get_worksheet_by_id(worksheet_id) # #gid sheet_id 890331805
+    data = worksheet.get_all_values()
+    df = pd.DataFrame(data)
+    df.columns = df.iloc[0] 
+    df = df[1:] 
+    return df
+
+
+cate_cols = ['Category_L1', 'Category_L2', 'Category_L3', 'Category_L4']
+def build_tree(df, current_level, max_level, path='', new_df=None):
+    if current_level == 0:
+        # Initialize the new DataFrame with the same index as the original
+        new_df = pd.DataFrame(index=df.index)
+    
+    if current_level >= max_level:
+        return [], new_df
+
+    next_level = current_level + 1
+    tree = []
+
+    category_col = f'Category_L{current_level + 1}'
+    title_col = f'{category_col}_Title'
+
+    for index, item in enumerate(df[cate_cols[current_level]].unique()):
+        if pd.notna(item):
+            # Create a path label with leading numbers
+            new_path = f'{path}{index:02d}_'
+
+            # Filter the DataFrame for the current category item
+            filtered_df = df[df[cate_cols[current_level]] == item]
+            
+            # Recursive call to build children and update new DataFrame
+            children, new_df = build_tree(filtered_df, next_level, max_level, new_path, new_df)
+
+            # Add category and title information to the new DataFrame
+            new_df.loc[filtered_df.index, category_col] = item
+            new_df.loc[filtered_df.index, title_col] = f'{new_path}{item} ({cate_cols[current_level]})'
+
+            node = {
+                'value': f'{new_path}{item} ({cate_cols[current_level]})',
+                'title': item,
+                'children': children
+            }
+            if not node['children']:
+                del node['children']
+            tree.append(node)
+
+    return tree, new_df
+
 
 class pressureHome:
     def __init__(self,path):
--- pages/01_Bar_view_🏗️.py
+++ pages/01_Bar_view_🏗️.py
@@ -1,38 +1,98 @@
 from helper import *
+from st_ant_tree import st_ant_tree
+import os
+
+st.header('Diageo pressure data')
+metabolic_colors_20 = ['#a63246' ,'#e56e61' ,'#ea9243' ,'#ffc840' ,'#789b40' ,'#bdd576','#f6e67f' ,'#2db77f' ,'#61c6c0' ,'#61cbe8' ,'#d87fae' ,'#9772a3' ,'#1f6585' ,'#b6517f' ,'#a89778' ,'#eae4b1' ,'#a2a9ad' ,'#a65b4a' ,'#82766a' ,'#797c82']
+metabolic_colors_40 = ['#a63246' ,'#cd3e49' ,'#e56e61' ,'#d16d29' ,'#ea9243' ,'#d8a83b' ,'#ffc840' ,'#5f8943' ,'#789b40' ,'#87bf65' ,'#bdd576' ,'#e2e26a' ,'#f6e67f' ,'#288e5b' ,'#2db77f' ,'#43a89a' ,'#61c6c0' ,'#a9dde3' ,'#61cbe8' ,'#60adcd' ,'#d87fae' ,'#a4a9d5' ,'#9772a3' ,'#775285' ,'#1f6585' ,'#2e839e' ,'#b6517f' ,'#a09256' ,'#a89778' ,'#c4b36a' ,'#eae4b1' ,'#f5f3ea' ,'#a2a9ad' ,'#b7744f' ,'#a65b4a' ,'#7b5d52' ,'#82766a' ,'#8c9380' ,'#797c82']
 
 
-st.header('this is overview page')
+SHEET_NAME = 'Phase1_pressures'
+WORK_SHEET_NAME = 'Long_data_with_pressures'
+SHEET_ID = 890331805
+
+# pressure_url = 'https://docs.google.com/spreadsheets/d/1TPyl6dFr1rSBDuAHksywHxUJIUIaup94wIB2CJMh2xQ/edit#gid=890331805'
+# sheet_name = 'Long_data_with_pressures'
+
+cate_cols = ['Category_L1','Category_L2','Category_L3','Category_L4']
+# index_cols: numerical, show in the bar x axis
+index_cols = ['Converted crop weight (tonnes)', 
+              'Global Warming Potential (kg CO2-Eq)', 'Air Pollution (kg PM2.5-Eq)',
+              'Land Use (m2)', 'Water Pollution (kg P eq)',
+              'Land Transformation (m2)', 'Soil Pollution (kg SO2 eq)',
+              'Water Use (m3)']
+final_col = f'{cate_cols[-1]}_Title'
+
+# sheet_id = sheet_url_split(pressure_url)
+# pressure = read_sheet(sheet_id,sheet_name)
+
+# pressure = pd.read_csv('./pages/Phase1_pressures - Long_data_with_pressures.csv')
+
+# pressure = read_credentials_sheet(SHEET_NAME,SHEET_ID)
+conn = st.connection("gsheets",type=GSheetsConnection)
+pressure = conn.read(worksheet=WORK_SHEET_NAME)
+
+with st.expander('pressure data frame: '):
+       st.dataframe(pressure.head())
+
+# tree data accroding to cate_cols
+tree_data, tree_data_df = build_tree(pressure,0,len(cate_cols))
+tree_data_df = tree_data_df.loc[:,[i for i in tree_data_df.columns.tolist() if i not in cate_cols]]
+pressure_new = pd.concat([tree_data_df,pressure],axis=1)
+# st.dataframe(pressure_new.head())
 
 
-bar_data = []
-C4_in = [] # ['Agave','Aniseed','Barley','Berries','Broken Rice']
-col_in = [] # ['Quantity','Land Use (m2)','Land Transformation (m2)']
+with st.container():
+       value = st_ant_tree(treeData=tree_data, filterTreeNode= True, 
+                     allowClear=True, max_height=700,min_height_dropdown=120,
+                     multiple= True, placeholder= "filtering categories", 
+                     showArrow= True, showSearch= True, treeCheckable= True)
 
-C4_in = st.sidebar.multiselect(
-    'Select Categoriy_L4',
-    all_C4,
-    all_C4)
 
-col_in = st.sidebar.multiselect(
-    'Select index',
-    all_col,
-    all_col)
+# st.write(value) # value: list : ['leaf1', 'leaf2', 'leaf3']
+on = st.toggle('bar summing up = 100%')
 
-for i in C4_in:
-        bar_data.append(go.Bar(name=i, x=col_in, 
-                            y=test1_ra.loc[test1_ra['Category_L4_LCA']==i,col_in].iloc[0,:].tolist(),
-                            marker_color = color_map[i]
-                            ))
+if value:
+    if on:
+       # st.dataframe(pressure_new.loc[pressure_new[final_col].isin(value),:])
+       test = pressure_new.loc[pressure_new[final_col].isin(value),:]
+       for i in index_cols:
+              test[i] = pd.to_numeric(test[i], errors='coerce')
+       test1 = test.groupby([final_col]).sum() 
+       test1_ra = test1.div(test1.sum(axis=0),axis=1).reset_index() 
 
-fig = go.Figure(data = bar_data)
-fig.update_layout(barmode='stack'
-                )
-fig.update_layout(width=800,
-                height=800,
-                xaxis_title='',
-                yaxis_title='%',
-                template=metabolic_template,
-                # name='My Legend Name'
-                )
+    else:
+       test = pressure_new.copy()
+       for i in index_cols:
+             test[i] = pd.to_numeric(test[i], errors='coerce')
+       test = test.groupby([final_col]).sum()
+       test1 = test.div(test.sum(axis=0),axis=1).reset_index() 
+       test1_ra = test1.loc[test1[final_col].isin(value),:]
+    
+    # matching long and short name for the last category
+    match_df = pressure_new.loc[pressure_new[final_col].isin(value),
+                                   [cate_cols[-1],final_col]]
+    match_df = match_df.drop_duplicates()
+    bare_name_d = dict(zip(match_df[final_col].tolist(),
+                           match_df[cate_cols[-1]].tolist()))
+    bar_data = []
+    for ind,i in enumerate(value):
+            bar_data.append(
+                  go.Bar(name=bare_name_d[i], x=index_cols, 
+                         y=test1_ra.loc[test1_ra[final_col]==i,index_cols].iloc[0,:].tolist(),
+                         marker_color = metabolic_colors_40[ind]
+                                ))
 
-st.plotly_chart(fig, use_container_width=True)
+    fig = go.Figure(data = bar_data)
+    fig.update_layout(barmode='stack')
+    fig.update_layout(width=800,
+                    height=800,
+                    xaxis_title='',
+                    yaxis_title='%',
+                    template=metabolic_template,
+                    margin=dict(l=10, r=10, t=10, b=10)
+                    # name='My Legend Name'
+                    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
